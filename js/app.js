00a7c6fd1b2b4069c7400c0d8a7a30a1dba2b5f1
Nabeel <nabeelhassan786@outlook.com>
2023-06-18 20:57:39 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,99 @@
+let side_bar = document.querySelector('.side-bar-main');
+
+let  div_slider_button = document.querySelector(".div-slider-button")
+
+
+
+$(window).ready (function(){
+    console.log('Loading....')
+    // $('.sec-banner').animate({'top':0 + "%" },700);
+    $('html, body').animate({
+        scrollTop:0
+    },100)
+})
+
+
+
+function menu_open(){
+
+if (side_bar.classList.contains('side-bar-main-inactive')){
+    side_bar.classList.add('side-bar-main-active')
+    side_bar.classList.remove('side-bar-main-inactive')
+    div_slider_button.classList.add('hidden')
+    div_slider_button.classList.remove('show')
+}else{
+
+    side_bar.classList.remove('side-bar-main-active')
+    side_bar.classList.add('side-bar-main-inactive')
+    div_slider_button.classList.add('show')
+    div_slider_button.classList.remove('hidden')
+}
+}
+
+// side bar toggle end
+
+
+
+console.log( window.innerHeight)
+
+let sec_banner = document.querySelector('.sec-banner')
+let sec2 = document.querySelector('.sec2')
+let sec3 = document.querySelector('.sec3')
+
+window.onscroll = function(event){
+
+    let scroll = window.pageYOffset;
+
+if (scroll > 60){
+
+        sec_banner.classList.add('slide-up')
+        
+        console.log("sec-banner")
+        
+            } else if (scroll < 200){
+
+                sec_banner.classList.add('slide-down')
+                sec_banner.classList.remove('slide-up')
+
+            }
+
+            if (scroll > 200){
+
+                sec2.classList.add('slide-up')
+                
+                console.log(sec2)
+                
+                    } else if (scroll < 340){
+                        console.log("down")
+                        sec2.classList.add('slide-down')
+                        sec2.classList.remove('slide-up')
+        
+                    }
+
+
+                    if (scroll > 340){
+
+                        sec3.classList.add('slide-up')
+                        
+                        console.log(sec3)
+                        
+                            } else if (scroll < 1800){
+                                console.log("down")
+                                sec3.classList.add('slide-down')
+                                sec3.classList.remove('slide-up')
+                
+                            }
+}
+
+
+
+// slider scrolling button
+
+
+function slidedown() {
+    window.scrollBy(0, 140);
+  }
+
+  function slideup() {
+    window.scrollBy(0, -140);
+  }
